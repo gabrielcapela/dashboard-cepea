f7cea0b543d7a21bea7fd152a6ae1efe75bcff0d
Gabriel Capela <gabrielcapela02@gmail.com>
2025-06-27 07:56:32 -0300

    prep to deploy

---
+++ scripts/update_all.py
@@ -0,0 +1,6 @@
+# update_all.py
+import subprocess
+
+subprocess.run(["python", "scripts/cepea_scraper.py"])
+subprocess.run(["python", "scripts/insert_data.py"])
+subprocess.run(["python", "scripts/modeling.py"])
