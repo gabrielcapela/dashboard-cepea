ff8b61505a986889e95cd308b61e356aef9c35ed
Gabriel Capela <gabrielcapela02@gmail.com>
2025-07-04 18:45:55 -0300

    editing my scraper

--- scripts/cepea_scraper.py
+++ scripts/cepea_scraper.py
@@ -25,7 +25,16 @@ end_date = current_date.strftime('%d/%m/%Y')
 
 # --- WEBDRIVER ---
 chrome_options = Options()
-#chrome_options.add_argument("--window-size=1920x1080")
+
+chrome_options.add_argument("--headless=new") 
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--window-size=1920,1080")
+
+
+
+
 chrome_options.add_experimental_option("prefs", {
     "download.default_directory": os.path.abspath(DOWNLOAD_FOLDER),
     "download.prompt_for_download": False,
